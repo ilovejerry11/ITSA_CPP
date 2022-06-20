a20529569f8f303933d3b0520c22608c6dcc598f
ilovejerry11 <86518582+ilovejerry11@users.noreply.github.com>
2022-06-21 03:14:54 +0800

    Update st22.c

--- st22.c
+++ st22.c
@@ -1,29 +1,21 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
 
 int main() {
-    
-    char str[300];
-    scanf("%s", str);
-
-    char convert = 'a' - 'A';
-    for (int i=0; i<strlen(str); i++) {
-        if (str[i] >= 'A' && str[i] <= 'Z') {
-            str[i] = str[i] + convert;
-        } else if (str[i] >= 'a' && str[i] <= 'z') {
-            str[i] = str [i] - convert;
+    string s;
+    cin >> s;
+    string ss(s.rbegin(), s.rend());
+    for (int i=0; i<ss.size(); i++) {
+        if (ss[i] >= 'a' && ss[i] <= 'z') {
+            cout << (char)toupper(ss[i]);
+        } else {
+            cout << (char)tolower(ss[i]);
         }
     }
-
-    int front = 0, rear = strlen(str) - 1;
-    while (front < rear) {
-        char temp = str[front];
-        str[front] = str[rear];
-        str[rear] = temp;
-
-        front++, rear--;
-    }
-    printf("%s\n", str);
-    
+    cout << endl;
 }
